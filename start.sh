77fad4a479ce95ef322157e744ac83c5490371d0
Jeff Baitis <jeff@baitis.net>
2018-05-13 16:09:00 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ samp_rate=$1
 center_freq=$2
 ppm=$3
 rf_gain=$4
-ssh -v root@192.168.8.250 "rtl_sdr -s $samp_rate -f $center_freq -p $ppm -g $rf_gain - | nc -u 192.168.8.22 9009" &
+ssh -v root@192.168.8.250 "rtl_sdr -b 131072 -s $samp_rate -f $center_freq -p $ppm -g $rf_gain - | nc -u 192.168.8.22 9009" &
+#ssh -v root@192.168.8.250 "rtl_sdr -s $samp_rate -f $center_freq -p $ppm -g $rf_gain - | nc -u 192.168.8.22 9009" &
 while sleep 0.1; do nc -u -l 9009 ; done
